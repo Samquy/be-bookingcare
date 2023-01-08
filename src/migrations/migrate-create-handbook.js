081bb3f64827d6c2a36f8741b8ce78aed3a41fd7
Samquy <slbaoquy@gmail.com>
2023-01-08 21:20:22 +0700

    initial commit

---
+++ src/migrations/migrate-create-handbook.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('Handbooks', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            name: {
+                type: Sequelize.STRING
+            },
+            image: {
+                type: Sequelize.BLOB('long')
+            },
+            contentHTML: {
+                type: Sequelize.TEXT('long'),
+                allowNull: true,
+            },
+            contentMarkdown: {
+                type: Sequelize.TEXT('long'),
+                allowNull: true,
+            },
+            userId: {
+                type: Sequelize.INTEGER,
+                allowNull: true,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('Handbooks');
+    }
+};

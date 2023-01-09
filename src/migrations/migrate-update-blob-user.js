fd1e76b9d1ecd1c39b814209979c885999a11386
Samquy <95400808+Samquy@users.noreply.github.com>
2023-01-09 19:58:44 +0700

    Update migrate-update-blob-user.js

--- src/migrations/migrate-update-blob-user.js
+++ src/migrations/migrate-update-blob-user.js
@@ -1,19 +1,19 @@
-// module.exports = {
-//     up: (queryInterface, Sequelize) => {
-//         return Promise.all([
-//             queryInterface.changeColumn('Users', 'image', {
-//                 type: Sequelize.BLOB('long'),
-//                 allowNull: true,
-//             })
-//         ])
-//     },
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return Promise.all([
+            queryInterface.changeColumn('Users', 'image', {
+                type: Sequelize.BLOB('long'),
+                allowNull: true,
+            })
+        ])
+    },
 
-//     down: (queryInterface, Sequelize) => {
-//         return Promise.all([
-//             queryInterface.changeColumn('Users', 'image', {
-//                 type: Sequelize.BLOB('long'),
-//                 allowNull: true,
-//             })
-//         ])
-//     }
-// };
+    down: (queryInterface, Sequelize) => {
+        return Promise.all([
+            queryInterface.changeColumn('Users', 'image', {
+                type: Sequelize.BLOB('long'),
+                allowNull: true,
+            })
+        ])
+    }
+};

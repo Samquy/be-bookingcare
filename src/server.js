49b71bf04af1a56fb964d227c6ceab77a8096e89
Samquy <95400808+Samquy@users.noreply.github.com>
2023-01-10 17:42:04 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,15 +7,15 @@ import cors from 'cors'
 require('dotenv').config();
 
 let app = express();
-const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
+// const cors=require("cors");
+// const corsOptions ={
+//    origin:'*', 
+//    credentials:true,            //access-control-allow-credentials:true
+//    optionSuccessStatus:200,
+// }
 
-app.use(cors(corsOptions)) // Use this after the variable declaration
-// app.use(cors({ credentials: true, origin: true }))
+// app.use(cors(corsOptions)) // Use this after the variable declaration
+app.use(cors({ credentials: true, origin: '*',optionSuccessStatus:200, }))
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 

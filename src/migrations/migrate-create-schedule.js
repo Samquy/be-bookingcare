4b57933fbe604cf426d3a6c2719a7366a4b78368
Samquy <95400808+Samquy@users.noreply.github.com>
2023-01-11 19:23:09 +0700

    Update migrate-create-schedule.js

--- src/migrations/migrate-create-schedule.js
+++ src/migrations/migrate-create-schedule.js
@@ -24,22 +24,13 @@ module.exports = {
                 type: Sequelize.INTEGER
             },
             createdAt: {
-                type: Sequelize.DATEONLY,
-                field: 'createdat',
+                allowNull: false,
+                type: Sequelize.DATE
             },
             updatedAt: {
-                type: Sequelize.DATEONLY,
-                field: 'updatedat'
-                },
-
-//             createdAt: {
-//                 allowNull: false,
-//                 type: Sequelize.DATE
-//             },
-//             updatedAt: {
-//                 allowNull: false,
-//                 type: Sequelize.DATE
-//             }
+                allowNull: false,
+                type: Sequelize.DATE
+            }
         });
     },
     down: async (queryInterface, Sequelize) => {
